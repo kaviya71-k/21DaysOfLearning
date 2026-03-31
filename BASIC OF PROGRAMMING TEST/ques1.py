197198ba99958816f4b2b0ca99642f0996ca41aa
Kaviya71 <kaviyakarthi71@gmail.com>
2026-03-31 19:24:03 +0530

    Add automated loan approval logic in ques1.py
    Implement automated loan approval system with credit score checks and income verification.

---
+++ BASIC OF PROGRAMMING TEST/ques1.py
@@ -0,0 +1,21 @@
+print("Automated loan approval system")
+creditscore=int(input("enter the credit amount(in percentage out of 100)"))
+income=int(input("Enter the income"))
+emi=int(input("enter the emi percentage"))
+employment_status=input("choose you employment status:(1.Salaried  2.Self employed   3.Unemployed  4.other)"))
+interest"N/A"
+if creditscore<600 and income<25000 and emi>50:
+  interest="N/A"
+  print("The application is rejected",interest)
+elif 650<=creditscore<=749:
+  print("it requires a manual review")
+elif creditscore>=750: 
+  if eligibity==Salaried or eligibilty==Self employed:
+    print("Loan is approved")
+    if creditscore>=800:
+      interest=(7*creditscore)/100
+    elif 750<creditscore<=799:
+      interest=(8*creditscore)/100
+print("The loan is approved",interest)
+  
+                
