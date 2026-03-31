ae4593c5c1eb1d4a7ad946f6371cb321b10f7e37
Kaviya71 <kaviyakarthi71@gmail.com>
2026-03-31 19:53:03 +0530

    Implement API rate limiter in ques3.py

---
+++ ques3.py
@@ -0,0 +1,9 @@
+print("API rate limiter")
+request_count=int(input("enter the number of request made by the user"))
+if request_count<=5:
+  status="Allowed"
+  print(f"Status:{status}")
+else:
+  status="Blocked"
+  print(f"Status:{status},Rate limit exceeded")
+  
