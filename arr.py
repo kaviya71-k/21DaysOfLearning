98b1f1d0c77f5cf333a67e414f72a7ce4e1bd05a
Kaviya <iamkaviya71@gmail.com>
2026-06-06 23:03:54 +0530

    Add files via upload

---
+++ arr.py
@@ -0,0 +1,13 @@
+from array import *
+a=array('i',[1,2,3,4,5])
+print("The elemnets are: ")
+for i in a:
+    print(i)
+print("__________________")
+print("Indexing:")
+from array import *
+a=array('i',[1,2,3,4,5])
+n=len(a)
+print("The elemnets are: ")
+for i in range(n):
+    print(a[i],end=' ')
---
+++ slicing.py
@@ -0,0 +1,14 @@
+from array import *
+a=array('i',[1,2,3,4,5,6,7,8,9])
+y=a[1:4]
+print(y)
+y=a[0:]
+print(y)
+y=a[:4]
+print(y)
+y=a[-4:]
+print(y)
+y=a[-4:-1]
+print(y)
+y=a[0:8:2]
+print(y)
